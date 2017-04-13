f7da81b8cf9b24ea97606c4fef2f6fd40432974d
lavender21 <1573872488@qq.com>
2017-04-13 21:25:53 +0800

    Generate 2km < distance < 8km and no waiting time case

--- main/taxi_fee.js
+++ main/taxi_fee.js
@@ -6,6 +6,8 @@ function calculateTaxiFee(distance, waitingTime) {
     let taxiFee = 0;
     if (distance <= 2){
         taxiFee = 6;
+    }else if(distance > 2 && distance < 8){
+        taxiFee = Math.round((distance-2) * 0.8 + 6);
     }
     return taxiFee;
 }
--- test/taxi_fee_spec.js
+++ test/taxi_fee_spec.js
@@ -2,10 +2,16 @@
  * Created by lavender on 17-4-13.
  */
 describe("A suit for taxi fee function", function () {
-    it("return expect result when distance less than 2km and 0 waiting time", function () {
+    it("return expect result when distance less than 2km and no waiting time", function () {
         const distance = 1;
         const waitingTime = 0;
         const result = calculateTaxiFee(distance, waitingTime);
         expect(result).toBe(6);
     });
+    it("return expect result when distance great than 2km less than 8km and no waiting time",function () {
+        const distance = 5;
+        const waitingTime = 0;
+        const result = calculateTaxiFee(distance, waitingTime);
+        expect(result).toBe(8);
+    })
 });
