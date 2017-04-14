5a53bec9c5c5e9b79f97ed040022ef176417504e
lavender21 <1573872488@qq.com>
2017-04-14 11:11:25 +0800

    Student management:add printMenu function and pass test

--- main/student_management.js
+++ main/student_management.js
@@ -1,3 +1,12 @@
 /**
  * Created by lavender on 17-4-14.
  */
+let allStudentInfo = [];
+
+function printMenu() {
+    console.log(`1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：`);
+}
+
--- test/student_management_test.js
+++ test/student_management_test.js
@@ -1,8 +1,15 @@
 /**
  * Created by lavender on 17-4-14.
  */
-describe("", function () {
-   it("", function () {
+describe("A suit for printMenu function", function () {
+   it("console menu list when called printMenu", function () {
+        const result = `1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：`;
+        spyOn(console,'log');
+        printMenu();
+        expect(console.log).toHaveBeenCalledWith(result);
 
    });
 });
